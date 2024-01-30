f04c9e5bd54b5fde47c262d30086664925fc4ead
kamarbaraka <kamar254baraka@gmail.com>
2024-01-30 14:41:45 +0300

    excluded sensitive data.
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- authServer/src/main/resources/schema.sql
+++ authServer/src/main/resources/schema.sql
@@ -1,2 +1,12 @@
+-- check and create a database for this server
+do $$
+    begin if not exists(
+        select from pg_catalog.pg_database where datname='hanjam_auth_db'
+    ) then perform dblink_exec(
+                   'dbname=postgres', 'create database hanjam_auth_db'
+                   ); end if ;
+    end
+    $$;
+
 -- check if a schema exists and create if not
 create schema if not exists auth_server;
